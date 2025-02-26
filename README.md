9ab43524610f511c578a28d4d5d5e145180083b3
Frankie <1426203851@qq.com>
2025-02-26 16:55:30 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# uniapp-simple-demo
+
+## 快速开始
+
+```shell
+$ pnpm install
+```
+
+```shell
+# mp-weixin
+$ pnpm dev:mp-weixin
+
+# h5
+$ pnpm dev:h5
+```
+
+> 编译到微信小程序的花，导入至微信开发工具，使用测试号打开即可。
---
+++ src/pages/index/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Sub from './sub.vue'
+</script>
+
+<template>
+  <view class="box">
+    父组件 600 x 600px，红色
+    <Sub />
+  </view>
+</template>
+
+<style scoped>
+:global(page) {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.box {
+  width: 600rpx;
+  height: 600rpx;
+  background-color: red;
+  margin: 100rpx auto 0;
+  color: #fff;
+}
+</style>
